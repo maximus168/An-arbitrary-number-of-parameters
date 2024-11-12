ff62c6b04180f1852f75f2db5b0a7cb0ffa2218f
maximus168 <mikula-77@mail.ru>
2024-11-12 05:31:04 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -2,13 +2,17 @@ def single_root_words(root_word, *other_words):
     root_word = root_word.lower()
     same_words = []
     for word in other_words:
-        if root_word in word.lower() or word in root_word:
+        if root_word in word.lower() or word in root_word.lower():
             same_words.append(word)
 
-        # return same_words
-    print(same_words)
+    return same_words
+result = single_root_words('зА', 'АзАзелло', 'ЗАбор ','Замер', 'запас', 'узник', 'пузатый')
+print(result)
 
-single_root_words('ЗА', 'АзАзелло','ЗАбор ' , 'запас', 'узник', 'пузатый')
+
+
+
+# single_root_words('зА', 'АзАзелло','ЗАбор ' , 'запас', 'узаконенный', 'узник', 'пузатый')
 
 
 
